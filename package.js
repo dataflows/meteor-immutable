58645ff2e41b357a641402ce709f7ea5f0a30e19
Andy Lash <andy@squidnet.com>
2015-08-17 09:46:57 -0700

    Explicitly export Immutable on the client side.  Without this this package works in an app, but doesn't work on the client side when used in a package

--- package.js
+++ package.js
@@ -13,5 +13,6 @@ Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.0.1");
   api.addFiles("vendor/Immutable.js", "client");
   api.addFiles("lib/require-immutable.js", "server");
+  api.addFiles("lib/export-immutable.js", "client");
   api.export("Immutable", ["server", "client"]);
 });
